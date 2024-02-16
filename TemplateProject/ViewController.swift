241b897a09ea2a58a9c55a852fab71d2f4fe8615
Roman Zakharchenko <r.zaharchenko23@gmail.com>
2024-02-17 01:22:53 +0200

    add cocoapod and realm, execute first task

--- TemplateProject/ViewController.swift
+++ TemplateProject/ViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var a: Int?
+    @IBOutlet weak var textFieldUserName: UITextField!
+    @IBOutlet weak var textFieldUserSurname: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        textFieldUserName.text = UserDefaults.standard.string(forKey: "textFieldUserName")
+        textFieldUserSurname.text = UserDefaults.standard.string(forKey: "textFieldUserSurname")
+    }
+
+    @IBAction
+    func textFieldNameEditingEnd(_ sender: Any) {
+        UserDefaults.standard.setValue(textFieldUserName.text, forKey: "textFieldUserName")
+    }
+
+    @IBAction
+    func textFieldSurnameEditingEnd(_ sender: Any) {
+        UserDefaults.standard.setValue(textFieldUserSurname.text, forKey: "textFieldUserSurname")
     }
 }
