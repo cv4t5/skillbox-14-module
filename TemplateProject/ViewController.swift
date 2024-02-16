316acb28749bb7e9611d7bd07ce4983f050c09e3
Roman Zakharchenko <r.zaharchenko23@gmail.com>
2024-02-16 23:03:05 +0200

    init

---
+++ TemplateProject/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  TemplateProject
+//
+//  Created by Роман Захарченко on 07.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var a: Int?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
